367e138b1603ed1ba9d5c1b9349f33ffab50c432
karolina <s198193@student.pg.edu.pl>
2025-05-20 00:33:01 +0200

    Initial MKA_Demo_App + report in polish

---
+++ MKA_Demo_App/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MKA_Demo_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button btn = new Button();
+            btn.Text = "Click.";
+            btn.Click += (sender, e) => MessageBox.Show("You clicked the button. ");
+            btn.Dock = DockStyle.Fill;
+            this.Controls.Add(btn);
+        }
+    }
+}
---
+++ MKA_Demo_App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MKA_Demo_App
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // Upewnij się, że przestrzeń nazw się zgadza
+        }
+    }
+}
